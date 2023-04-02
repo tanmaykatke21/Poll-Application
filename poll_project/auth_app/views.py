c100aba6ba306471fd93151b5c9949d3db2394bc
Tanmay <katketanmay2102@gmail.com>
2023-04-03 05:12:02 +0530

    User Authentication App made

--- poll_project/auth_app/views.py
+++ poll_project/auth_app/views.py
@@ -1,10 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
 
 def ulogin(request):
-	return render(request,"ulogin.html")
+	if request.user.is_authenticated:
+		return redirect("uwelcome")
+	else:
+		if request.method == "POST":
+			un = request.POST.get("un")
+			pw = request.POST.get("pw")
+			usr = authenticate(username=un,password=pw)
+			if usr is None:
+				return render(request,"ulogin.html",{"msg":"invalid username/password"})
+			else:
+				login(request,usr)
+				return redirect("uwelcome")
+		else:	
+			return render(request,"ulogin.html")
 
 def usignup(request):
-	return render(request,"usignup.html")
+	if request.user.is_authenticated:
+		return redirect("uwelcome")
+	else:
+		if request.method == "POST":
+			un = request.POST.get("un")
+			pw1 = request.POST.get("pw1")
+			pw2 = request.POST.get("pw2")
+			if pw1 == pw2:
+				try:
+					usr = User.objects.get(username=un)
+					return render(request,"usignup.html",{"msg":"user already exists"})
+				except User.DoesNotExist:
+					usr = User.objects.create_user(username=un,password=pw1)
+					usr.save()
+					return redirect("ulogin")
+			else:
+				return render(request,"usignup.html",{"msg":"password did not match"})
+		else:
+			return render(request,"usignup.html")
 
 def uwelcome(request):
-	return render(request,"uwelcome.html")
+	if request.user.is_authenticated:
+		return render(request,"uwelcome.html")
+	else:
+		return redirect("ulogin")
+
+def ulogout(request):
+	logout(request)
+	return redirect("ulogin")
+
+def ucp(request):
+	if request.user.is_authenticated:
+		if request.method == "POST":
+			pw1 = request.POST.get("npw1")
+			pw2 = request.POST.get("npw2")
+			pw = User.objects.get(password=request.user.password)
+			if (pw1==pw2):
+				try:
+					usr = User.objects.get(username=request.user.username)
+					usr.set_password(pw1)
+					usr.save()
+					return redirect("ulogin")
+				except User.DoesNotExist:
+					return render(request,"ucp.html",{"msg":"User does not exists"})
+			else:
+				return render(request,"ucp.html",{"msg":"password did not match"})
+		else:
+			return render(request,"ucp.html")
+	else:
+		return redirect("ulogin")			
--- poll_project/poll_project/urls.py
+++ poll_project/poll_project/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from auth_app.views import ulogin, usignup, uwelcome
+from auth_app.views import ulogin, usignup, uwelcome, ulogout, ucp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('',ulogin,name="ulogin"),
 	path('usignup',usignup,name='usignup'),
 	path('uwelcome',uwelcome,name='uwelcome'),
+	path('ulogout',ulogout,name='ulogout'),
+	path('ucp',ucp,name='ucp'),
 ]
